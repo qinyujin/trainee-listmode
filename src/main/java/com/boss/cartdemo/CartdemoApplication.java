409a8f6849347812e0053e96652b2105f10aa4fb
yujinqin <522350817@qq.com>
2020-07-16 09:33:51 +0800

    Initial commit

---
+++ src/main/java/com/boss/cartdemo/CartdemoApplication.java
@@ -0,0 +1,13 @@
+package com.boss.cartdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartdemoApplication.class, args);
+    }
+
+}
